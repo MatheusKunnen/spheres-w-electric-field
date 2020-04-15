4499d7930fb2f1888b9a6dad2e90eca844f430db
MatheusKunnen <matheuskl@accionproactiva.com>
2020-04-15 14:34:23 -0400

    Extra Functions
    - Variable dt
    - Code cleaned

--- src/Main.py
+++ src/Main.py
@@ -1,4 +1,3 @@
-# @desc: Sphere Elastic Collisions Simulator
 # @autor: Matheus Kunnen Ledesma - matheusl.2000@alunos.utfpr.edu.br
 
 import numpy as np
@@ -19,37 +18,43 @@ import math
 
 class Main:
     # General parameters
-    DEBUG = True
-    ROT_K = 1.
+    ROT_K = .5
     MOVE_K = .1
+    DT_K = .1
 
     # Simulator parameters
     ring_v = []
 
     def __init__(self):
-        # Init General Var
+        # Init General Parameters
         self.is_running = False
         self.is_paused = True
         self.dt = 1/60.0
+        self.dt_k = 1.
         self.target_dt = 1/30.0
+        self.t_total = 0
+        self.hud_enabled = True
+
+        # Init Bodies
+        self.init_bodies()
+
+        # Init Graphics Manager
+        self.g_manager = OpenGLManager("Sphere in a Ring's Electric Field | Matheus Kunnen ")
         self.move_vector = np.array([0., 0., 0.])
         self.rot_vector = np.array([0., 0., 0.])
-        self.t_total = 0
-        # Init Bodies
-        self.body = Body(1, .1, 1, np.array(
-            [0, 0, 0]), np.array([0, 0, 2]), -10)
-        self.ring = Ring(np.array([0, 0, 0]), 10, 320)
-        self.E = np.array([0., 0., 0.])
-        # Init Graphics Manager
-        self.g_manager = OpenGLManager("Electric Field")
         self.cam_pos = np.array([0, 20, -5])
         self.cam_rot = np.array([-40, 0, 0])
         self.g_manager.cam_pos = self.cam_pos
         self.g_manager.cam_rot = self.cam_rot
 
+    def init_bodies(self):
+        self.body = Body(1, .1, 1, np.array([0, 0, 0]), np.array([0, 0, 2]), -10)
+        self.ring = Ring(np.array([0, 0, 0]), 10, 320)
+        self.E = np.array([0., 0., 0.])
+
     def run(self):
         self.is_running = self.g_manager.init_display()
-
+        self.t_total = 0
         while self.is_running:
             t1 = time.time()
             self.g_manager.clear_buffer()
@@ -68,29 +73,32 @@ class Main:
 
     def update(self):
         self.E = self.ring.get_electric_field(self.body.b_pos)
-        self.body.b_aceleration = self.E * \
-            (self.body.b_charge/self.body.b_mass)
-        self.body.update(self.dt)
+        self.body.b_aceleration = self.E * (self.body.b_charge/self.body.b_mass)
+        self.body.update(self.dt*self.dt_k)
 
     def draw_hud(self):
+        if not self.hud_enabled:
+            return
         txt_status = "Paused" if self.is_paused else "Running"
         self.g_manager.captions = [
             "-> General Parameters",
-            f"       FPS: {round(1/self.dt,0)}",
-            f"Cam. dPos.: {np.round(self.cam_pos, 2)}",
-            f"Cam. dRot.: {np.round(self.cam_rot, 2)}",
-            f"Status: {txt_status}", "",
+            f"    FPS: {round(1/self.dt,0)}",
+            f"Runtime: {round(self.t_total, 3)}s.",
+            f"   Play: x{round(self.dt_k,1)}",
+            f"Cam. dP: {np.round(self.cam_pos, 2)}",
+            f"Cam. dR: {np.round(self.cam_rot, 2)}",
+            f" Status: {txt_status}", "",
             "-> Central Body ",
-            f"   P: {np.round(self.body.b_pos, 3)}",
-            f"   V: {np.round(self.body.b_vel, 3)}",
-            f"   A: {np.round(self.body.b_aceleration, 3)}",
-            f"E(P): {np.round(self.E, 3)}", "",
+            f"     P: {np.round(self.body.b_pos, 3)}",
+            f"     V: {np.round(self.body.b_vel, 3)}",
+            f"     A: {np.round(self.body.b_aceleration, 3)}",
+            f"  E(P): {np.round(self.E, 3)}", "",
             "-> Ring",
             f"     P: {np.round(self.ring.r_pos, 3)}",
             f"Radius: {round(self.ring.r_radius,3)}",
             f"     Q: {round(self.ring.r_charge,3)}",
-            f"N. Spheres: {round(self.ring.n_bodies,3)}",
-            f" Q Spheres: {round(self.ring.q_bodies,3)}"]
+            f"N. Sph: {round(self.ring.n_bodies,3)}",
+            f" Q Sph: {round(self.ring.q_bodies,3)}"]
         self.g_manager.draw_captions()
 
     def update_dt(self, t1, t2):
@@ -116,41 +124,31 @@ class Main:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN or event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT:
-                    self.move_vector = [
-                        Main.MOVE_K, 0., 0.] if event.type == pygame.KEYDOWN else [0., 0., 0.]
-                    # self.g_manager.move_cam()
+                    self.move_vector[0] = Main.MOVE_K if event.type == pygame.KEYDOWN else 0.
                 elif event.key == pygame.K_RIGHT:
-                    self.move_vector = [
-                        -Main.MOVE_K, 0., 0.] if event.type == pygame.KEYDOWN else [0., 0., 0.]
+                    self.move_vector[0] = - Main.MOVE_K if event.type == pygame.KEYDOWN else 0.
                 elif event.key == pygame.K_DOWN:
-                    self.move_vector = [
-                        0., Main.MOVE_K, 0.] if event.type == pygame.KEYDOWN else [0., 0., 0.]
+                    self.move_vector[1] = Main.MOVE_K if event.type == pygame.KEYDOWN else 0.
                 elif event.key == pygame.K_UP:
-                    self.move_vector = [
-                        0., -Main.MOVE_K, 0.] if event.type == pygame.KEYDOWN else [0., 0., 0.]
+                    self.move_vector[1] = - Main.MOVE_K if event.type == pygame.KEYDOWN else 0.
                 elif event.key == pygame.K_q:
-                    self.move_vector = [
-                        0., 0., Main.MOVE_K] if event.type == pygame.KEYDOWN else [0., 0., 0.]
+                    self.move_vector[2] = Main.MOVE_K if event.type == pygame.KEYDOWN else 0.
                 elif event.key == pygame.K_e:
-                    self.move_vector = [
-                        0., 0., -Main.MOVE_K] if event.type == pygame.KEYDOWN else [0., 0., 0.]
+                    self.move_vector[2] = - Main.MOVE_K if event.type == pygame.KEYDOWN else 0.
                 elif event.key == pygame.K_a:
-                    self.rot_vector = [
-                        0., -Main.ROT_K, 0.] if event.type == pygame.KEYDOWN else [0., 0., 0.]
+                    self.rot_vector[1] = - Main.ROT_K if event.type == pygame.KEYDOWN else 0.
                 elif event.key == pygame.K_d:
-                    self.rot_vector = [
-                        0., Main.ROT_K, 0.] if event.type == pygame.KEYDOWN else [0., 0., 0.]
+                    self.rot_vector[1] = Main.ROT_K if event.type == pygame.KEYDOWN else 0.
                 elif event.key == pygame.K_w:
-                    self.rot_vector = [
-                        -Main.ROT_K, 0., 0.] if event.type == pygame.KEYDOWN else [0., 0., 0.]
+                    self.rot_vector[0] = - Main.ROT_K if event.type == pygame.KEYDOWN else 0.
                 elif event.key == pygame.K_s:
-                    self.rot_vector = [
-                        Main.ROT_K, 0., 0.] if event.type == pygame.KEYDOWN else [0., 0., 0.]
-                elif event.key == pygame.K_PAGEUP and c_simulator.dt < .2:
-                    pass
-                elif event.key == pygame.K_PAGEDOWN and c_simulator.dt > .01:
-                    pass
-                    # c_simulator.dt = c_simulator.dt - .01
+                    self.rot_vector[0] = Main.ROT_K if event.type == pygame.KEYDOWN else 0.  
+                elif event.key == pygame.K_h and event.type == pygame.KEYDOWN:
+                    self.hud_enabled = not self.hud_enabled
+                elif event.key == pygame.K_PAGEUP and event.type == pygame.KEYDOWN:
+                    self.dt_k += Main.DT_K
+                elif event.key == pygame.K_PAGEDOWN and event.type == pygame.KEYDOWN and round(self.dt_k - Main.DT_K,1) > 0:
+                    self.dt_k -= Main.DT_K
                 elif event.key == pygame.K_p and event.type == pygame.KEYDOWN:
                     self.is_paused = not self.is_paused
             if event.type == pygame.QUIT:
@@ -164,16 +162,8 @@ class Main:
         self.g_manager.move_cam(self.move_vector)
         self.g_manager.rotate_cam(self.rot_vector)
 
+if __name__ == "__main__":
+    main = Main()
+    main.run()
 
-main = Main()
-main.run()
-a = np.array([1, 1, 1])
-b = np.array([2, 1, 4])
-print(a)
-print(b)
-a = a * 1.5
-b = b * 5.3
-c = a + b
-print(a)
-print(b)
-print(c)
+exit()
--- src/OpenGLManager.py
+++ src/OpenGLManager.py
@@ -1,6 +1,7 @@
 # @author: Matheus Kunnen Ledesma
 
 import math
+import numpy as np
 
 import pygame
 from pygame.locals import *
@@ -15,8 +16,9 @@ class OpenGLManager:
     display_size = (1200, 800)  # Tamanho da janela a abrir
     sphere_slices = 10          # Divisioes das bolas (> -> Maior Qualidade)
     text_pos = (10, 750)        # Posicao inicial do texto
-    text_dP = 175                # Distancia entre linhas do texto
+    text_dP = 175               # Distancia entre linhas do texto
     D_RENDER_DISTANCE = 100     # Distancia maxima de renderizado
+    STROKE_W = 2.               # Tamanho de Linhas
     """ Ilumination parameters """
     # Posicao da fonte de iluminacao
     LIGHT_ZERO_POSITION = [0, 0, 24, .5]
@@ -36,6 +38,8 @@ class OpenGLManager:
     camera_rot_y = 30.
 
     """ Colors """
+    COLOR_BLACK = [0., 0., 0., 1.]
+    COLOR_WHITE = [1., 1., 1., 1.]
     cube_color = [1., 0., 0., 1.]                       # Cor do frame do Cubo
     vector_color = [1, 1, 1, 1]             # Cor da esfera
     # Cor da sombra da esfera
@@ -46,62 +50,59 @@ class OpenGLManager:
     sphere_collision_diffuse_color = [1., .0, 0., 1.]   # Cor das bolas
     sphere_collision_ambient_color = [.5, .0,
                                       0., .1]   # Cor ambiente? das bolas
-    # text_color = [10., 10., 10., 1.]                    # Cor do texto
-    text_color = [0., 0., 0., 1000.]                    # Cor do texto
 
-    def __init__(self, display_title):
+    def __init__(self, display_title, bg_color=COLOR_WHITE):
         self.running = False
         self.display_title = display_title
         self.render_distance = OpenGLManager.D_RENDER_DISTANCE
+        self.bg_color = np.array(bg_color)
         self.captions = []
+        self.init_colors()
+
+    def init_colors(self):
+        self.vector_color = self.text_color = self.p_color = np.array([1., 1., 1., 2.]) - self.bg_color
+         
 
     def init_display(self):
         if self.running:
             return True
-
+        # Init Window
         pygame.init()
         pygame.display.set_mode(
-            self.display_size, DOUBLEBUF | OPENGL)
+            self.display_size, pygame.DOUBLEBUF | OPENGL)
         pygame.display.set_caption(self.display_title)
         pygame.display.gl_set_attribute(GL_ACCELERATED_VISUAL, True)
-        glClearColor(0.1, 0.1, 0.1, 1.)
+        # Config window
+        glClearColor(self.bg_color[0], self.bg_color[1],
+                     self.bg_color[2], self.bg_color[3])
         glShadeModel(GL_SMOOTH)
         glEnable(GL_CULL_FACE)
         glEnable(GL_DEPTH_TEST)
         glEnable(GL_LIGHTING)
         glCullFace(GL_BACK)
-
+        # Init light
         glLightfv(GL_LIGHT0, GL_AMBIENT, self.LIGHT_ZERO_AMBIENT)
         glLightfv(GL_LIGHT0, GL_POSITION, self.LIGHT_ZERO_POSITION)
         glLightfv(GL_LIGHT0, GL_DIFFUSE, self.LIGHT_ZERO_COLOR)
         glLightfv(GL_LIGHT0, GL_SPECULAR, self.LIGHT_ZERO_SPECULAR)
-
         glLightf(GL_LIGHT0, GL_CONSTANT_ATTENUATION, 0.5)
         glLightf(GL_LIGHT0, GL_LINEAR_ATTENUATION, 0.01)
         glEnable(GL_LIGHT0)
 
+        # Init Camera
         glMatrixMode(GL_PROJECTION)
         gluPerspective(
             45, (self.display_size[0] / self.display_size[1]), 0.1, self.render_distance)
 
         glMatrixMode(GL_MODELVIEW)
-        glClearColor(1, 1, 1, 1)
         glPushMatrix()
 
-        # Init camera
+        # Pos Camera
         self.set_cam_pos(self.cam_pos)
         self.rotate_cam(self.cam_rot)
 
         self.running = True
         return True
-
-    def setup_cam(self, cam_pos, cam_rot):
-        glMatrixMode(GL_MODELVIEW)
-        glPopMatrix()
-        glPushMatrix()
-        glRotatef(-60, 1., 0., 0.)
-        glTranslatef(cam_pos[0], cam_pos[1], cam_pos[2])
-        self.viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
 
     def set_cam_pos(self, cam_pos):
         self.cam_pos = cam_pos
@@ -109,17 +110,6 @@ class OpenGLManager:
         glPopMatrix()
         glPushMatrix()
         glTranslatef(cam_pos[0], cam_pos[1], cam_pos[2])
-        self.viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
-
-    def set_rot_cam(self, cam_rot):
-        # self.cam_rot = cam_rot
-        glMatrixMode(GL_MODELVIEW)
-        glPopMatrix()
-        glPushMatrix()
-        # glTranslatef(self.cam_pos[0], self.cam_pos[1], self.cam_pos[2])
-        glRotatef(cam_rot[0], 1., 0., 0.)
-        glRotatef(cam_rot[1], 0., 1., 0.)
-        glRotatef(cam_rot[2], 0., 0., 1.)
         self.viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
 
     def move_cam(self, cam_move):
@@ -135,6 +125,7 @@ class OpenGLManager:
         # apply rotation
         glRotatef(self.cam_rot[0], 1., 0., 0.)
         glRotatef(self.cam_rot[1], 0., 1., 0.)
+
         # multiply the current matrix by the get the new view matrix and store the final vie matrix
         glMultMatrixf(self.viewMatrix)
         self.viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
@@ -152,17 +143,10 @@ class OpenGLManager:
         glRotatef(cam_rot[0], 1., 0., 0.)
         glRotatef(cam_rot[1], 0., 1., 0.)
 
-        # init the view matrix
-        # glPushMatrix()
-        # glLoadIdentity()
-
         # multiply the current matrix by the get the new view matrix and store the final vie matrix
         glMultMatrixf(self.viewMatrix)
         self.viewMatrix = glGetFloatv(GL_MODELVIEW_MATRIX)
 
-        # apply view matrix
-        # glPopMatrix()
-       # glMultMatrixf(self.viewMatrix)
 
     def clear_buffer(self):
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
@@ -183,6 +167,10 @@ class OpenGLManager:
         glMatrixMode(GL_MODELVIEW)
         glPushMatrix()
         glLoadIdentity()
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable(GL_BLEND)
+        glEnable(GL_LINE_SMOOTH)
+        glLineWidth(OpenGLManager.STROKE_W)
         glTranslatef(self.text_pos[0], self.text_pos[1], 0)
         glScalef(0.1, 0.1, 0.1)
         glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, self.text_color)
@@ -190,31 +178,21 @@ class OpenGLManager:
             glPushMatrix()
             glTranslatef(0,
                          - i*self.text_dP, 0)
-            # glTranslatef(self.text_pos[0],
-            #              self.text_pos[1] - i*self.text_dP, 0.)
-            #glRasterPos2d(self.text_pos[0], self.text_pos[1] - i*self.text_dP)
             for j in range(len(caption)):
                 glutStrokeCharacter(GLUT_STROKE_MONO_ROMAN,
                                     ord(caption[j]))
-                # glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24,ord(self.captions[i][j]))
             glPopMatrix()
 
         """ Making sure we can render 3d again """
         glMatrixMode(GL_PROJECTION)
         glPopMatrix()
         glMatrixMode(GL_MODELVIEW)
-        # --self.set_cam_pos(self.cam_pos)
-        # --self.set_rot_cam(self.cam_rot)
-        # glTranslatef(cam_pos[0], cam_pos[1], cam_pos[2])
-        # glRotatef(camera_rot_x, 1., 0., 0.)
-        # glRotatef(camera_rot_y, 0., 1., 0.)
         glPopMatrix()
 
     def draw_solid_sphere(self, sphere_pos, sphere_radius, sphere_ambient_color=sphere_ambient_color):
         glPushMatrix()
-        K = 1
+        K = 1.5
         glMaterialfv(GL_FRONT, GL_AMBIENT, sphere_ambient_color)
-        # glMaterialfv(GL_FRONT, GL_DIFFUSE, self.sphere_diffuse_color)
         glMaterialfv(GL_FRONT, GL_DIFFUSE, [
                      sphere_ambient_color[0]*K,
                      sphere_ambient_color[1]*K,
@@ -227,19 +205,24 @@ class OpenGLManager:
 
     def draw_cube_frame(self, cube_length, cube_color=cube_color):
         glPushMatrix()
-        glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, self.cube_color)
+        glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, cube_color)
         glTranslatef(0, 0, 0)
         glutWireCube(cube_length)
         glPopMatrix()
 
     def draw_vector(self, p_0, p_1):
         glPushMatrix()
-        glMaterialfv(GL_FRONT, GL_AMBIENT, self.vector_color)
-
+        # Config stroke
+        glMaterialfv(GL_FRONT, GL_AMBIENT, self.p_color)
+        glEnable(GL_BLEND)
+        glEnable(GL_LINE_SMOOTH)
+        glLineWidth(OpenGLManager.STROKE_W)
+        # Draw Stroke
         glBegin(GL_LINES)
         glVertex3f(p_0[0], p_0[1], p_0[2])
         glVertex3f(p_0[0]+p_1[0], p_0[1]+p_1[1], p_0[2]+p_1[2])
         glEnd()
+        # Draw Point
         glTranslatef(p_0[0]+p_1[0], p_0[1]+p_1[1], p_0[2]+p_1[2])
         glutSolidSphere(.005, self.sphere_slices, self.sphere_slices)
         glPopMatrix()
